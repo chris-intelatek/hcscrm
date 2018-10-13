5f8980f99ecce5d45d31d9e43fe6113a92c425aa
Chris Cunningham <chris@intelatek.com>
2018-10-13 18:13:42 +0000

    production mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,5 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
-  
+  config.action_mailer.default_url_options = { host: "hcs-crm.herokuapp.com"}
 end
