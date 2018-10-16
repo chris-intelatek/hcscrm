e051f910ded85103f40690ade38f2e1aab756162
Chris Cunningham <chris@intelatek.com>
2018-10-16 16:21:57 +0000

    email hcsq

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,7 @@
 class NotificationMailer < ApplicationMailer
     def new_hcsq(prospect)
         @prospect = prospect
-        mail(to: [prospect.user_email, 'chris@intelatek.com'],
+        mail(to: [prospect.user_email, 'bluecoast@90degreebenefitstx.com', 'chris@bluecoastsavings.com'],
              subject: 'New HCSQ Submitted')
     end
     
