12edeacae2b401115e59e93a7a57d7b96486f4ea
Chris Cunningham <chris@intelatek.com>
2018-10-19 23:19:45 +0000

    update mail and static pages

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -13,7 +13,7 @@ class NotificationMailer < ApplicationMailer
     
     def new_comment(comment)
         @comment = comment
-        mail(to: comment.prospect.user_email,
+        mail(to: [comment.prospect.user_email, 'chris@bluecoastsavings.com'],
              subject: 'New Comment Submitted')
     end
 end
