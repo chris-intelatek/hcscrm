8fef32d386ab89334515efc6a69587bca14bc032
Chris Cunningham <chris@intelatek.com>
2018-10-22 22:48:36 +0000

    allow frames and add collections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  
+  #Added this method to allow for display in frames 
+  def iframe_action
+    response.headers.delete "X-Frame-Options"
+    render_something
+  end
+  
 end
