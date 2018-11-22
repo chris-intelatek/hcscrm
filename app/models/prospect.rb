8b208a329e5d1861e72d3ca22ac1c5862df751f2
Chris Cunningham <chris@intelatek.com>
2018-11-22 08:05:36 +0000

    Validate employees as integer

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -2,7 +2,7 @@ class Prospect < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :dependent => :delete_all
   validates :organization, :presence => true
-
+  validates :employees, :numericality => {:only_integer => true}
   delegate :email, to: :user, allow_nil: true, prefix: true
 
 
@@ -19,8 +19,6 @@ class Prospect < ActiveRecord::Base
   end
 
 
-
-  
   def self.search(query)
     # Note that PostgreSQL is case specific for like when searching where sqlite3 is not
     where("LOWER(organization) like ?", "%#{query.downcase}%")
