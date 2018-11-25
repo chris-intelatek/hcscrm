74db1230144afca201eb65ee6ab3ad6ec3a687ce
Chris Cunningham <chris@intelatek.com>
2018-11-25 06:18:25 +0000

    add update column to csv

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -10,11 +10,42 @@ class Prospect < ActiveRecord::Base
   def self.to_csv
     CSV.generate(headers: true) do |csv|
       csv << ["Advisor Name", "Advisor Email", "Advisor Phone", "Advisor Mobile",
-      "Organization", "Intro Pres Date", "SME Call Date", "Status"]
+      "Organization", "Update", "Intro Pres Date", "SME Call Date", "Status"]
       all.each do |prospect|
+        
+        #<!-- I wrote this very clunky section that I need to rework at some point in the future... Chris
+      	if prospect.next_action_date.nil? || prospect.next_action_date < Date.yesterday
+          update1 = "yes"
+      	end
+        if prospect.intro_presentation_date.nil? || prospect.intro_presentation_date < Date.yesterday
+          update2 = "yes"
+        end
+        if prospect.hcs_sme_fact_finding_call_date.nil? || prospect.hcs_sme_fact_finding_call_date < Date.yesterday
+          update3 = "yes"
+        end
+      	if prospect.hcs_sme_proposal_meeting_date.nil? || prospect.hcs_sme_proposal_meeting_date < Date.yesterday
+          update4 = "yes"
+      	end
+      	if prospect.effective_date.nil? || prospect.effective_date < Date.yesterday
+          update5 = "yes"
+      	end
+    	  if (prospect.status != "Other - No Sale") && 
+    	        (prospect.status != "Closed - Group is Effective") && 
+    	        (prospect.status !=  "Intro Pres Given - No Sale") && 
+    	        (prospect.status != "Fact Finder Call Done - No Sale") && 
+    	        (prospect.status != "Proposal Given - No Sale") && 
+    	        (prospect.status != "Not Qualified - No Sale")
+          if update1 && update2 && update3 && update4 && update5 == "yes"
+        	  update = "Needs Updating"
+        	  else
+        	    update = "ok"
+          end
+    	  end
+    		#<!--End of clunky section.-->
+        
         advisor_name = prospect.user.advisor_first_name + " " + prospect.user.advisor_last_name
         csv << [advisor_name, prospect.user.email, prospect.user.advisor_phone,
-              prospect.user.advisor_mobile, prospect.organization, prospect.intro_presentation_date,
+              prospect.user.advisor_mobile, prospect.organization, update, prospect.intro_presentation_date,
               prospect.hcs_sme_fact_finding_call_date, prospect.status]
       end
     end
