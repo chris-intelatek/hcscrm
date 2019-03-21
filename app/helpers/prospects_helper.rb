f4295f96a8eca7dd824a7b70310d7516db0208dd
Chris Cunningham <chris@intelatek.com>
2019-03-21 15:25:19 +0000

    dashboard

--- app/helpers/prospects_helper.rb
+++ app/helpers/prospects_helper.rb
@@ -2,15 +2,6 @@ module ProspectsHelper
 
   # Dashboard
   
-  def all_in_crm 
-    @prospects.count
-  end
-
-  def hcs_prospects_in_crm
-		@prospects.where(hcs_prospect: true).count +
-		@prospects.where.not(status: nil).count
-  end
-
   def hcs_no_sale
     @prospects.where(status: "Intro Pres Given - No Sale").count +
     @prospects.where(status: "Fact Finder Call Done - No Sale").count +
@@ -20,10 +11,10 @@ module ProspectsHelper
   end
 
   def pay_no_sale
-    @prospects.where(status: "Intro Pres Given - No Sale").count +
-    @prospects.where(status: "SME Presentation Done - No Sale").count +
-    @prospects.where(status: "Not Qualified").count +
-    @prospects.where(status: "Other - No Sale").count
+    @prospects.where(pay_status: "Intro Pres Given - No Sale").count +
+    @prospects.where(pay_status: "SME Presentation Done - No Sale").count +
+    @prospects.where(pay_status: "Not Qualified").count +
+    @prospects.where(pay_status: "Other - No Sale").count
   end
 
 end
