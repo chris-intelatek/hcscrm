35c71ef419b74715738a514931e4db275f9cbcb5
Chris Cunningham <chris@intelatek.com>
2019-03-25 23:59:26 +0000

    temp csv import

--- app/models/prospect.rb
+++ app/models/prospect.rb
@@ -63,13 +63,8 @@ class Prospect < ActiveRecord::Base
 
   #Import CSV file
   def self.import(file)
-    spreadsheet = open_spreadsheet(file)
-    header = spreadsheet.row(1)
-    (2..spreadsheet.last_row).each do |i|
-      row = Hash[[header, spreadsheet.row(i)].transpose]
-      prospect = find_by_id(row["id"]) || new
-      prospect.attributes = row.to_hash.slice(*accessible_attributes)
-      prospect.save!
+    CSV.foreach(file.path, headers: true) do |row|
+      Prospect.create! row.to_hash
     end
   end
 
