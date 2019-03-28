e6521cd07834a4c3b792caf5b18dd0857c7069d3
Chris Cunningham <chris@intelatek.com>
2019-03-28 23:03:10 +0000

    update mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,5 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
-  config.action_mailer.default_url_options = { host: "crm.healthcaresavings.co"}
+  config.action_mailer.default_url_options = { host: "bluecoastcrm.com"}
 end
