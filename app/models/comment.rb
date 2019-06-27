0a265f5cc86c6f712a9fdff4469bcd8d63323d8a
Chris Cunningham <chris@intelatek.com>
2019-06-27 13:04:21 +0000

    fix comment edit

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :prospect
   belongs_to :user
+  
+  def content_field
+    content_fields = ['vcp_content', 'hcs_content', 'pay_content']
+    content_fields.select{ |field| attributes[field].present? }.first
+  end
 end
