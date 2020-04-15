a0ca526b01144441ede91510dc841926a01d3f9a
Chris Cunningham <chris@intelatek.com>
2020-04-15 19:55:04 +0000

    vendor limits

--- app/controllers/prospects_controller.rb
+++ app/controllers/prospects_controller.rb
@@ -7,12 +7,15 @@ class ProspectsController < ApplicationController
     @shipping_opportunity_status = Prospect.select(:shipping_opportunity_status).order(:shipping_opportunity_status).distinct
     @hcs_opportunity_status = Prospect.select(:hcs_opportunity_status).order(:hcs_opportunity_status).distinct
     @pay_status = Prospect.select(:pay_status).order(:pay_status).distinct
+    @vcp_status = Prospect.select(:vcp_status).order(:vcp_status).distinct    
     if params[:shipping_opportunity_status] != nil
       @prospects = Prospect.where(shipping_opportunity_status: params[:shipping_opportunity_status])
     elsif params[:hcs_opportunity_status] != nil
       @prospects = Prospect.where(hcs_opportunity_status: params[:hcs_opportunity_status])
     elsif params[:pay_status] != nil
       @prospects = Prospect.where(pay_status: params[:pay_status])
+    elsif params[:vcp_status] != nil
+      @prospects = Prospect.where(vcp_status: params[:vcp_status])
     elsif params[:search]
       @prospects = Prospect.search(params[:search]).order("created_at DESC")
     elsif
@@ -146,7 +149,7 @@ class ProspectsController < ApplicationController
   def hcs_update
     if @prospect.update(prospect_params)
       flash[:success] = "Prospect has been updated."
-      redirect_to prospect_path
+      redirect_to entrust_path
     else
       render 'hcs'
     end
@@ -174,17 +177,17 @@ class ProspectsController < ApplicationController
   end
 
 
-  # def entrust_lead_status
-  # end
+  def hcs_lead_status
+  end
 
-  # def entrust_lead_status_update
-  #   if @prospect.update(prospect_params)
-  #     flash[:success] = "Lead has been updated."
-  #     redirect_to shipstore_path
-  #   else
-  #     render 'shipping_lead_status_path'
-  #   end
-  # end
+  def hcs_lead_status_update
+    if @prospect.update(prospect_params)
+      flash[:success] = "Lead has been updated."
+      redirect_to entrust_path
+    else
+      render 'hcs_lead_status_path'
+    end
+  end
 
 
 
@@ -282,7 +285,7 @@ class ProspectsController < ApplicationController
 
   private
     def prospect_params
-      params.require(:prospect).permit(:user_id, :organization, :street_address, :address2, :city, :state, :zip, :phone, :website, :employees, :prospect_notes, :created_at, :updated_at, :intro_presentation_date, :hcs_sme_fact_finding_call_date, :current_health_benefit_cost, :new_estimated_health_benefit_cost, :savings_percentage, :agreement_date, :hcs_sme_proposal_meeting_date, :contact1_first_name, :contact1_last_name, :contact1_title, :contact1_phone, :contact1_mobile, :contact1_email, :contact2_first_name, :contact2_last_name, :contact2_title, :contact2_phone, :contact2_mobile, :contact2_email, :primary_contact, :intro_presenter, :hcs_sme, :close_date, :effective_date, :hcbq_business_type, :hcbq_no_of_eligible_employees, :hcbq_total_participation, :hcbq_current_carrier, :hcbq_current_coverage_type, :hcbq_most_important, :hcbq_sustainable, :hcbq_oop, :hcbq_like_best, :hcbq_like_least, :hcbq_scale, :hcbq_perfect_plan, :hcbq_notes, :intelatek_bda, :intelatek_notes, :intelatek_day, :intelatek_timezone, :hcs_monthly_savings_fee, :hcs_pepm_fee, :intelatek_time, :submitted_hcsq, :hcs_next_action, :hcs_next_action_date, :hcs_prospect, :pay_prospect, :vcp_prospect, :pay_intro_presentation_date, :pay_intro_presenter, :pay_sme_presentation_date, :pay_sme, :pay_agreement_date, :pay_hourly_employees, :pay_next_action_date, :pay_next_action, :pay_submitted_to_branch, :hcs_opportunity_status, :pay_status, :renewal_month, :hcs_notes, :pay_notes, :date_submitted_to_branch, :date_submitted_to_branch, :intelatek_lead_type, :vcp_intro_presentation_date, :vcp_intro_presenter, :vcp_sme_presentation_date, :vcp_supplier_match_meeting_date, :vcp_sme, :vcp_status, :vcp_agreement_date, :vcp_next_action_date, :vcp_next_action, :vcp_notes, :shipping_prospect, :annual_revenue, :shipping_contact_first_name, :shipping_contact_last_name, :shipping_contact_title, :shipping_contact_direct_phone, :shipping_contact_mobile, :shipping_contact_email, :shipping_type, :shipping_annual_parcel_spend, :shipping_total_spend, :shipping_carrier_provided_system, :shipping_interest_in_rate_analysis, :shipping_parcels_per_day, :shipping_integration, :shipping_notes, :shipping_opportunity_status, :shipping_lead_status, :shipping_next_action, :shipping_next_action_date)
+      params.require(:prospect).permit(:user_id, :organization, :street_address, :address2, :city, :state, :zip, :phone, :website, :employees, :prospect_notes, :created_at, :updated_at, :intro_presentation_date, :hcs_sme_fact_finding_call_date, :current_health_benefit_cost, :new_estimated_health_benefit_cost, :savings_percentage, :agreement_date, :hcs_sme_proposal_meeting_date, :contact1_first_name, :contact1_last_name, :contact1_title, :contact1_phone, :contact1_mobile, :contact1_email, :contact2_first_name, :contact2_last_name, :contact2_title, :contact2_phone, :contact2_mobile, :contact2_email, :primary_contact, :intro_presenter, :hcs_sme, :close_date, :effective_date, :hcbq_business_type, :hcbq_no_of_eligible_employees, :hcbq_total_participation, :hcbq_current_carrier, :hcbq_current_coverage_type, :hcbq_most_important, :hcbq_sustainable, :hcbq_oop, :hcbq_like_best, :hcbq_like_least, :hcbq_scale, :hcbq_perfect_plan, :hcbq_notes, :intelatek_bda, :intelatek_notes, :intelatek_day, :intelatek_timezone, :hcs_monthly_savings_fee, :hcs_pepm_fee, :intelatek_time, :submitted_hcsq, :hcs_next_action, :hcs_next_action_date, :hcs_prospect, :pay_prospect, :vcp_prospect, :pay_intro_presentation_date, :pay_intro_presenter, :pay_sme_presentation_date, :pay_sme, :pay_agreement_date, :pay_hourly_employees, :pay_next_action_date, :pay_next_action, :pay_submitted_to_branch, :hcs_opportunity_status, :pay_status, :renewal_month, :hcs_notes, :pay_notes, :date_submitted_to_branch, :date_submitted_to_branch, :intelatek_lead_type, :vcp_intro_presentation_date, :vcp_intro_presenter, :vcp_sme_presentation_date, :vcp_supplier_match_meeting_date, :vcp_sme, :vcp_status, :vcp_agreement_date, :vcp_next_action_date, :vcp_next_action, :vcp_notes, :shipping_prospect, :annual_revenue, :shipping_contact_first_name, :shipping_contact_last_name, :shipping_contact_title, :shipping_contact_direct_phone, :shipping_contact_mobile, :shipping_contact_email, :shipping_type, :shipping_annual_parcel_spend, :shipping_total_spend, :shipping_carrier_provided_system, :shipping_interest_in_rate_analysis, :shipping_parcels_per_day, :shipping_integration, :shipping_notes, :shipping_opportunity_status, :shipping_lead_status, :shipping_next_action, :shipping_next_action_date, :hcs_lead_status)
     end
 
 
