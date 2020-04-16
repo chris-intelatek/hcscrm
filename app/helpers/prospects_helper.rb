e73937982baf83b7f032baf4a471dda57642d278
Chris Cunningham <chris@intelatek.com>
2020-04-16 01:31:51 +0000

    fix dashboard

--- app/helpers/prospects_helper.rb
+++ app/helpers/prospects_helper.rb
@@ -1,13 +1,20 @@
 module ProspectsHelper
 
   # Dashboard
+
+
+  def shipping_no_sale
+    @prospects.where(shipping_opportunity_status: "Closed Lost").count +
+    @prospects.where(shipping_opportunity_status: "Closed Not Qualified").count +
+    @prospects.where(shipping_opportunity_status: "Lead Declined by ShipStore").count
+  end
   
   def hcs_no_sale
-    @prospects.where(status: "Intro Pres Given - No Sale").count +
-    @prospects.where(status: "Fact Finder Call Done - No Sale").count +
-    @prospects.where(status: "Proposal Given - No Sale").count +
-    @prospects.where(status: "Not Qualified").count +
-    @prospects.where(status: "Other - No Sale").count
+    @prospects.where(hcs_opportunity_status: "Intro Pres Given - No Sale").count +
+    @prospects.where(hcs_opportunity_status: "Fact Finder Call Done - No Sale").count +
+    @prospects.where(hcs_opportunity_status: "Proposal Given - No Sale").count +
+    @prospects.where(hcs_opportunity_status: "Not Qualified").count +
+    @prospects.where(hcs_opportunity_status: "Other - No Sale").count
   end
 
   def pay_no_sale
