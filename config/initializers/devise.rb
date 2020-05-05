c5f8453666a22f6bcb6821d912fc9f5f1dd62884
Chris Cunningham <chris@intelatek.com>
2020-05-05 15:58:17 +0000

    change devise email sender address

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "noreply@bluecoastsavings.com"
+  config.mailer_sender = "noreply@bluecoastcrm.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
