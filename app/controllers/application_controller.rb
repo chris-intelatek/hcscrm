283b74a883d166757468d3c7c86203d6a7d61078
Alfred Dominic <mailme@alfiemax.com>
2023-05-20 14:30:36 +0530

    make app load in replit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  #Added this method to allow for display in frames
-  after_action :allow_intelatek_frames
-  
- 
-  private
-  
-  def allow_intelatek_frames
-    response.headers['X-Frame-Options'] = 'ALLOWALL'
-  end
-  
 end
